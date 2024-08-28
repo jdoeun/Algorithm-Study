927a11d249159ab6eb3804cf9280c574c5088f67
iceprins <junghankyul@gmail.com>
2024-08-28 22:15:16 +0900

    feat: PSG_등대 알고리즘 풀이

---
+++ iceprins/240828/PSG_등대.py
@@ -0,0 +1,37 @@
+import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10 ** 6)
+
+
+def solution(n, lighthouse):
+    dp = [[0] * 2 for _ in range(n)]
+    node_dict = defaultdict(list)
+    visited = [False] * n
+
+    for a, b in lighthouse:
+        node_dict[a - 1].append(b - 1)
+        node_dict[b - 1].append(a - 1)
+
+    def dfs(node):
+        visited[node] = True
+        leaf_list = []
+
+        for leaf in node_dict[node]:
+            if visited[leaf]:
+                continue
+            leaf_list.append(leaf)
+            dfs(leaf)
+
+        dp[node][0] = 0
+        dp[node][1] = 1
+
+        for leaf in leaf_list:
+            dp[node][1] += min(dp[leaf])
+            dp[node][0] += dp[leaf][1]
+
+        return
+
+    dfs(0)
+
+    return min(dp[0])
